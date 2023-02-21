2acb6f4efce7d07c9b45e0c55d7ac9bdddd4ccdd
Muhammet Alkan <93428614+xulame@users.noreply.github.com>
2023-02-22 00:39:25 +0300

    Add files via upload

---
+++ SortAlgorithms/src/radixSort/Main.java
@@ -0,0 +1,60 @@
+package radixSort;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int[] radixArray = { 4725, 4586, 1330, 1594, 5729};
+		
+		radixSort(radixArray, 10, 4);
+		
+		
+		
+		
+		System.out.println(Arrays.toString(radixArray));
+		
+	}
+	
+	public static void radixSort(int[] input, int radix, int width) {
+		
+		for(int i = 0; i < width; i++) {
+			radixSingleSort(input,i,radix);
+		}
+		
+		
+	}
+	
+	public static void radixSingleSort(int input[], int position, int radix) {
+		
+		int numItems = input.length;
+		
+		int[] countArray = new int[radix];
+		
+		for(int value: input) {
+			countArray[getDigit(position,value,radix)]++;
+		}
+		
+		for(int j = 1; j < radix; j++) {
+			countArray[j] += countArray[j - 1];
+		}
+		
+		int[] temp = new int[numItems];
+		for(int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
+			temp[--countArray[getDigit(position, input[tempIndex], radix)]] = 
+					input[tempIndex];
+		}
+		
+		for(int tempIndex = 0; tempIndex < numItems; tempIndex++) {
+			input[tempIndex] = temp[tempIndex];
+		}
+		
+	}
+	
+	public static int getDigit(int position, int value, int radix) {
+		return value / (int) Math.pow(radix, position) % radix;
+	}
+	
+
+}
